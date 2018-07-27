346f676d18ba6e3fbaf07608c411c7a1aa12c97b
William Lawrence <williamjlawrence1@gmail.com>
2018-07-27 14:55:48 -0400

    added js functionality

--- script.js
+++ script.js
@@ -16,11 +16,19 @@ function getElementFromTemplate(id) {
 document.addEventListener('DOMContentLoaded', () => {
 
     // listen for a click on the get question button and gets the question.
-    document.querySelector('button#get-question').addEventListener('click', (event) => {
+    document.querySelector('button#start').addEventListener('click', (event) => {
         getNewQuestion();
+        
+
     });
 });
 
+let question_id;
+let score = 0; // this variable keeps track of the score as the number of the questions answered correctly.
+
+/**
+ * Gets a jeopardy question using the jservice API.
+ */
 function getNewQuestion() {
     const url = 'http://jservice.io/api/random';
     const settings = {
@@ -31,5 +39,35 @@ function getNewQuestion() {
     .then(response => response.json())
     .then(json => {
         console.log(json);
+        addQuestionToPage(json);
     });
+}
+
+/**
+ * Adds the question called from the API to the page.
+ * @param {Object} question the question to be added to the page.
+ */
+function addQuestionToPage(question) {
+    console.log('Adding question to page.');
+
+    // Creates a copy of the question template.
+    const questionTemplate = getElementFromTemplate('question-template');
+
+    // Reveals the score.
+    const p = document.querySelector('h2#score');
+    p.classList.remove('hidden')
+    
+    // Hides the start button.
+    document.querySelector('button#start').classList.add('hidden');
+
+    // Sets ths text of the question
+    questionTemplate.querySelector('p.question').innerText = question[0].question;
+    questionTemplate.querySelector('p.difficulty').innerText = question[0].value;
+    questionTemplate.querySelector('p.category').innerText = question[0].category.title;
+
+    document.querySelector('div#question').insertAdjacentElement('beforebegin', questionTemplate);
+
+
+
+
 }
